f1f84a8991956bd19d51763bd584f68cd6239467
biradarrameshwar <biradarrameshwar29@gmail.com>
2026-08-31 12:49:13 +0530

    Update RBB04.C

--- RBB04.C
+++ RBB04.C
@@ -5,9 +5,9 @@ Solution(2) -> User Define Values
 #include<stdio.h>
 int main()
 {
-float radius,area;
-printf("\n Enter the radius of the circle:");
-scanf("%f",& radius);
-area=(3.14*radius*radius);
-printf("\n Area of Circle=%f",area);
+ float radius,area;
+ printf("\n Enter the radius of circle:");
+ scanf("%f",& radius);
+ area=(3.14*radius*radius);
+ printf("\n Area of Circle=%f",area);
 }
