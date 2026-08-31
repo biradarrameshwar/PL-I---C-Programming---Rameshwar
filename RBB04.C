6da6ba386706679aba5623616f1023042531123c
biradarrameshwar <biradarrameshwar29@gmail.com>
2026-08-31 20:11:49 +0530

    Create RBB04.C

---
+++ RBB04.C
@@ -0,0 +1,16 @@
+/*
+Program (4) -> Write a program to calculate the area of a circle.
+Solution(2) -> User Define Values
+*/
+# include<stdio.h>
+int main()
+{
+  float radius,area;
+   
+  printf("\nEnter the radius ofcircle:");
+  scanf("%f",& radius);
+  area=(3.14*radius*radius);
+  printf("\n Area of Circle=%f",area);
+
+  return 0;
+}
